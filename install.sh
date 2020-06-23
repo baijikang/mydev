b1110fd3c92a977478edc36307169c9eeddaff56
baijikang <baijikang@163.com>
2020-06-23 23:11:14 +0800

    add package

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo '\n\n\napt 安裝基礎包'                                                
 sleep 2                                                                                                 && \
 apt update                                                                                              && \
 apt upgrade  -y                                                                                         && \
-apt install curl wget vim git net-tools iproute2 iputils-ping  python python3 fish htop axel -y         && \
+apt install curl wget vim git net-tools iproute2 iputils-ping  python python3 fish htop axel screen -y  && \
 echo '\n\n\napt 配置fish'                                                                               && \
 sleep 2                                                                                                 && \
 mkdir -p  ~/.config/fish/                                                                               && \
@@ -10,16 +10,17 @@ mv config.fish ~/.config/fish/config.fish                                       
 echo '              fish 已經配置好，以後可以用 /usr/bin/fish 作爲 docker 啓動命令 '                    && \
 echo '\n\n\n  * 下載 ndoejs'                                                                            && \
 sleep 2                                                                                                 && \
-wget -c  'https://nodejs.org/dist/v14.4.0/node-v14.4.0-linux-x64.tar'                                   && \
+wget -c  'https://npm.taobao.org/mirrors/node/v14.4.0/node-v14.4.0-linux-x64.tar.gz'                    && \
 mkdir -p ~/opt/                                                                                         && \
-tar -xf node-v14.4.0-linux-x64.tar                                                                      && \
-rm node-v14.4.0-linux-x64.tar                                                                           && \
+tar -xf node-v14.4.0-linux-x64.tar.gz                                                                   && \
+rm node-v14.4.0-linux-x64.tar.gz                                                                        && \
 mv node-v14.4.0-linux-x64 ~/opt/node                                                                    && \
 ln -s ~/opt/node/bin/node /usr/local/bin/node                                                           && \
 export PATH=/root/opt/node/bin:$PATH                                                                    && \
 echo '\n\n\n  * npm 全局安裝常用工具'                                                                   && \
 sleep 2                                                                                                 && \
-npm install -g pm2 live-server typescript yarn nodemon n @vue/cli                                       && \
+npm install -g yarn                                                                                     && \
+yarn global add pm2 live-server typescript nodemon n @vue/cli                                           && \
 
 
 echo '\n\n\n  * 下面請手工安裝 vim coc 插件'                                                                                                && \
