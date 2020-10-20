d02a4c849e339b3c0abcdc353742fc2e28883b6f
baijikang <baijikang@163.com>
2020-10-20 16:34:27 +0800

    update

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ mv config.fish ~/.config/fish/config.fish                                       
 echo '              fish 已經配置好，以後可以用 /usr/bin/fish 作爲 docker 啓動命令 '                    && \
 echo '\n\n\n  * 下載 ndoejs'                                                                            && \
 sleep 2                                                                                                 && \
-wget -c  'https://npm.taobao.org/mirrors/node/v14.4.0/node-v14.4.0-linux-x64.tar.gz'                    && \
+wget -c  'https://npm.taobao.org/mirrors/node/v14.14.0/node-v14.14.0-linux-x64.tar.gz'                  && \
 mkdir -p ~/opt/                                                                                         && \
-tar -xf node-v14.4.0-linux-x64.tar.gz                                                                   && \
-rm node-v14.4.0-linux-x64.tar.gz                                                                        && \
-mv node-v14.4.0-linux-x64 ~/opt/node                                                                    && \
+tar -xf node-v14.14.0-linux-x64.tar.gz                                                                  && \
+rm node-v14.14.0-linux-x64.tar.gz                                                                       && \
+mv node-v14.14.0-linux-x64 ~/opt/node                                                                   && \
 ln -s ~/opt/node/bin/node /usr/local/bin/node                                                           && \
 export PATH=/root/opt/node/bin:$PATH                                                                    && \
 echo '\n\n\n  * npm 全局安裝常用工具'                                                                   && \
@@ -26,6 +26,7 @@ yarn global add pm2 live-server typescript nodemon n @vue/cli                   
 echo '\n\n\n  * 下面請手工安裝 vim coc 插件'                                                                                                && \
 echo '         先切换到 fish 环境下运行'                                                                                                    && \
 echo '         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'        && \
+echo '         如果网络错误，就设置 hosts 在 www.ipaddress.com 查询 raw.githubusercontent.com 真实 host'                                    && \
 echo '        - 打開 vim '                                                                                                                  && \
 echo '        - 執行 :PlugInstall'                                                                                                          && \
 echo '        - 執行 :CocUpdate'                                                                                                            && \
